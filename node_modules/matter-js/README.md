66bad5e6de394039da1fdd04678e7b4bd7ca2646
Palmer Paul <pzpaul2002@yahoo.com>
2017-06-08 16:19:13 -0400

    add node_modules with dependencies for gh-pages

---
+++ node_modules/matter-js/README.md
@@ -0,0 +1,162 @@
+<img alt="Matter.js" src="http://brm.io/matter-js/img/matter-js.svg" width="300">
+
+> *Matter.js* is a JavaScript 2D rigid body physics engine for the web
+
+[brm.io/matter-js](http://brm.io/matter-js/)
+
+[Demos](#demos) - [Gallery](#gallery) - [Features](#features) - [Install](#install) - [Usage](#usage) - [Examples](#examples) -  [Docs](#documentation) - [Wiki](https://github.com/liabru/matter-js/wiki) - [References](#references) - [License](#license)
+
+[![Build Status](https://travis-ci.org/liabru/matter-js.png?branch=master)](https://travis-ci.org/liabru/matter-js)
+
+### Demos
+
+<table>
+  <tr>
+    <td>
+      <ul>
+        <li><a href="http://brm.io/matter-js/demo/#mixed">Mixed Shapes</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#mixedSolid">Solid Shapes</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#svg">Concave SVG Paths</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#terrain">Concave Terrain</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#concave">Concave Bodies</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#compound">Compound Bodies</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#newtonsCradle">Newton's Cradle</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#wreckingBall">Wrecking Ball</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#slingshot">Slingshot Game</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#rounded">Rounded Corners</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#views">Views</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#timescale">Time Scaling</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#manipulation">Body Manipulation</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#compositeManipulation">Composite Manipulation</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#raycasting">Raycasting</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#sprites">Sprites</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#pyramid">Pyramid</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#car">Car</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#catapult">Catapult</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#gravity">Reverse Gravity</a></li>
+      </ul>
+    </td>
+    <td>
+      <ul>
+        <li><a href="http://brm.io/matter-js/demo/#bridge">Bridge</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#avalanche">Avalanche</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#softBody">Basic Soft Bodies</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#cloth">Cloth</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#events">Events</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#collisionFiltering">Collision Filtering</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#chains">Chains</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#ballPool">Ball Pool</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#stack">Stack</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#circleStack">Circle Stack</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#compoundStack">Compound Stack</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#restitution">Restitution</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#friction">Friction</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#airFriction">Air Friction</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#staticFriction">Static Friction</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#sleeping">Sleeping</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#broadphase">Grid Broadphase</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#beachBalls">Beach Balls</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#stress">Stress 1</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#stress2">Stress 2</a></li>
+        <li><a href="http://brm.io/matter-js/demo/#sensors">Sensors</a></li>
+      </ul>
+      <br>
+    </td>
+  </tr>
+</table>
+
+### Gallery
+
+See how others are using matter.js physics
+
+- [Les métamorphoses de Mr. Kalia](http://lab212.org/Les-metamorphoses-de-Mr-Kalia) by Lab212 for Google
+- [4ify](http://pauliescanlon.io/4ify-channel-4-rebrand-2/) by Supernatural for Channel 4
+- [Adobe Analytics Live Stream](http://adobefirehose.mediarain.com/) by Rain for Adobe
+- [Blood Sweat & Tools Interactive](http://bloodsweatandtools.discovery.ca/gamebench/) by Jam3 for Discovery
+- [Oracle OpenWorld Experiment](http://theappslab.com/2016/10/10/how-i-attended-oracle-openworld-2016/) by Ed Jones for Oracle
+- [Pablo The Flamingo](http://pablotheflamingo.com/) by Nathan Gordon
+- [Goblins and Grottos](http://store.steampowered.com/app/389190) by Psychic Software
+- [masQueLaCara](https://www.instagram.com/p/BCFqCs6JNsq/) by Zach Lieberman for Art Blocks Houston
+- [#GIFMYLIVE](http://bonhommeparis.com/en/projects/arte-gifmylive) by Bonhomme for Arte
+- [Hype](http://tumult.com/hype/pro/) by Tumult
+- [more...](https://github.com/liabru/matter-js/wiki/Gallery)
+
+### Features
+
+- Rigid bodies
+- Compound bodies
+- Composite bodies
+- Concave and convex hulls
+- Physical properties (mass, area, density etc.)
+- Restitution (elastic and inelastic collisions)
+- Collisions (broad-phase, mid-phase and narrow-phase)
+- Stable stacking and resting
+- Conservation of momentum
+- Friction and resistance
+- Events
+- Constraints
+- Gravity
+- Sleeping and static bodies
+- Rounded corners (chamfering)
+- Views (translate, zoom)
+- Collision queries (raycasting, region tests)
+- Time scaling (slow-mo, speed-up)
+- Canvas renderer (supports vectors and textures)
+- [MatterTools](https://github.com/liabru/matter-tools) for creating, testing and debugging worlds
+- World state serialisation (requires [resurrect.js](https://github.com/skeeto/resurrect-js))
+- Cross-browser (Chrome, Firefox, Safari, IE8+)
+- Mobile-compatible (touch, responsive)
+- An original JavaScript physics implementation (not a port)
+
+### Install
+
+Download the [edge build (master)](https://github.com/liabru/matter-js/blob/master/build/matter.js) or get a [stable release](https://github.com/liabru/matter-js/releases) and include the script in your web page:
+
+    <script src="matter.js" type="text/javascript"></script>
+
+You can also install using the package managers [Bower](http://bower.io/search/?q=matter-js) and [NPM](https://www.npmjs.org/package/matter-js).
+
+    bower install matter-js
+    npm install matter-js
+
+### Usage
+
+Visit the [Getting started](https://github.com/liabru/matter-js/wiki/Getting-started) wiki page for a minimal usage example.  
+Also see the [Rendering](https://github.com/liabru/matter-js/wiki/Rendering) wiki page, which also shows how to use your own game loop.
+
+### Examples
+
+See the [examples](https://github.com/liabru/matter-js/tree/master/examples) directory which contains the source for all [demos](#demos).  
+There are even more examples on [codepen](http://codepen.io/collection/Fuagy/).
+
+### Documentation
+
+See the [API Documentation](http://brm.io/matter-js/docs/) and the [wiki](https://github.com/liabru/matter-js/wiki)
+
+### Building and Contributing
+
+To build you must first install [node.js](http://nodejs.org/) and [gulp](http://gulpjs.com/), then run
+
+	npm install
+
+This will install the required build dependencies, then run
+
+	gulp dev
+
+to spawn a development server. For information on contributing see [CONTRIBUTING.md](https://github.com/liabru/matter-js/blob/master/CONTRIBUTING.md).
+
+### Changelog
+
+To see what's new or changed in the latest version, see the [changelog](https://github.com/liabru/matter-js/blob/master/CHANGELOG.md).
+
+### References
+
+See the wiki page on [References](https://github.com/liabru/matter-js/wiki/References).
+
+### License
+
+Matter.js is licensed under [The MIT License (MIT)](http://opensource.org/licenses/MIT)  
+Copyright (c) 2014 Liam Brummitt
+
+This license is also supplied with the release and source code.  
+As stated in the license, absolutely no warranty is provided.
---
+++ node_modules/opentype.js/src/substitution.js
@@ -0,0 +1,301 @@
+// The Substitution object provides utility methods to manipulate
+// the GSUB substitution table.
+
+'use strict';
+
+var check = require('./check');
+var Layout = require('./layout');
+
+/**
+ * @exports opentype.Substitution
+ * @class
+ * @extends opentype.Layout
+ * @param {opentype.Font}
+ * @constructor
+ */
+var Substitution = function(font) {
+    Layout.call(this, font, 'gsub');
+};
+
+// Check if 2 arrays of primitives are equal.
+function arraysEqual(ar1, ar2) {
+    var n = ar1.length;
+    if (n !== ar2.length) { return false; }
+    for (var i = 0; i < n; i++) {
+        if (ar1[i] !== ar2[i]) { return false; }
+    }
+    return true;
+}
+
+// Find the first subtable of a lookup table in a particular format.
+function getSubstFormat(lookupTable, format, defaultSubtable) {
+    var subtables = lookupTable.subtables;
+    for (var i = 0; i < subtables.length; i++) {
+        var subtable = subtables[i];
+        if (subtable.substFormat === format) {
+            return subtable;
+        }
+    }
+    if (defaultSubtable) {
+        subtables.push(defaultSubtable);
+        return defaultSubtable;
+    }
+}
+
+Substitution.prototype = Layout.prototype;
+
+/**
+ * Create a default GSUB table.
+ * @return {Object} gsub - The GSUB table.
+ */
+Substitution.prototype.createDefaultTable = function() {
+    // Generate a default empty GSUB table with just a DFLT script and dflt lang sys.
+    return {
+        version: 1,
+        scripts: [{
+            tag: 'DFLT',
+            script: {
+                defaultLangSys: { reserved: 0, reqFeatureIndex: 0xffff, featureIndexes: [] },
+                langSysRecords: []
+            }
+        }],
+        features: [],
+        lookups: []
+    };
+};
+
+/**
+ * List all single substitutions (lookup type 1) for a given script, language, and feature.
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ * @param {string} feature - 4-character feature name ('aalt', 'salt', 'ss01'...)
+ * @return {Array} substitutions - The list of substitutions.
+ */
+Substitution.prototype.getSingle = function(feature, script, language) {
+    var substitutions = [];
+    var lookupTables = this.getLookupTables(script, language, feature, 1);
+    for (var idx = 0; idx < lookupTables.length; idx++) {
+        var subtables = lookupTables[idx].subtables;
+        for (var i = 0; i < subtables.length; i++) {
+            var subtable = subtables[i];
+            var glyphs = this.expandCoverage(subtable.coverage);
+            var j;
+            if (subtable.substFormat === 1) {
+                var delta = subtable.deltaGlyphId;
+                for (j = 0; j < glyphs.length; j++) {
+                    var glyph = glyphs[j];
+                    substitutions.push({ sub: glyph, by: glyph + delta });
+                }
+            } else {
+                var substitute = subtable.substitute;
+                for (j = 0; j < glyphs.length; j++) {
+                    substitutions.push({ sub: glyphs[j], by: substitute[j] });
+                }
+            }
+        }
+    }
+    return substitutions;
+};
+
+/**
+ * List all alternates (lookup type 3) for a given script, language, and feature.
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ * @param {string} feature - 4-character feature name ('aalt', 'salt'...)
+ * @return {Array} alternates - The list of alternates
+ */
+Substitution.prototype.getAlternates = function(feature, script, language) {
+    var alternates = [];
+    var lookupTables = this.getLookupTables(script, language, feature, 3);
+    for (var idx = 0; idx < lookupTables.length; idx++) {
+        var subtables = lookupTables[idx].subtables;
+        for (var i = 0; i < subtables.length; i++) {
+            var subtable = subtables[i];
+            var glyphs = this.expandCoverage(subtable.coverage);
+            var alternateSets = subtable.alternateSets;
+            for (var j = 0; j < glyphs.length; j++) {
+                alternates.push({ sub: glyphs[j], by: alternateSets[j] });
+            }
+        }
+    }
+    return alternates;
+};
+
+/**
+ * List all ligatures (lookup type 4) for a given script, language, and feature.
+ * The result is an array of ligature objects like { sub: [ids], by: id }
+ * @param {string} feature - 4-letter feature name ('liga', 'rlig', 'dlig'...)
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ * @return {Array} ligatures - The list of ligatures.
+ */
+Substitution.prototype.getLigatures = function(feature, script, language) {
+    var ligatures = [];
+    var lookupTables = this.getLookupTables(script, language, feature, 4);
+    for (var idx = 0; idx < lookupTables.length; idx++) {
+        var subtables = lookupTables[idx].subtables;
+        for (var i = 0; i < subtables.length; i++) {
+            var subtable = subtables[i];
+            var glyphs = this.expandCoverage(subtable.coverage);
+            var ligatureSets = subtable.ligatureSets;
+            for (var j = 0; j < glyphs.length; j++) {
+                var startGlyph = glyphs[j];
+                var ligSet = ligatureSets[j];
+                for (var k = 0; k < ligSet.length; k++) {
+                    var lig = ligSet[k];
+                    ligatures.push({
+                        sub: [startGlyph].concat(lig.components),
+                        by: lig.ligGlyph
+                    });
+                }
+            }
+        }
+    }
+    return ligatures;
+};
+
+/**
+ * Add or modify a single substitution (lookup type 1)
+ * Format 2, more flexible, is always used.
+ * @param {string} feature - 4-letter feature name ('liga', 'rlig', 'dlig'...)
+ * @param {Object} substitution - { sub: id, delta: number } for format 1 or { sub: id, by: id } for format 2.
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ */
+Substitution.prototype.addSingle = function(feature, substitution, script, language) {
+    var lookupTable = this.getLookupTables(script, language, feature, 1, true)[0];
+    var subtable = getSubstFormat(lookupTable, 2, {                // lookup type 1 subtable, format 2, coverage format 1
+        substFormat: 2,
+        coverage: { format: 1, glyphs: [] },
+        substitute: []
+    });
+    check.assert(subtable.coverage.format === 1, 'Ligature: unable to modify coverage table format ' + subtable.coverage.format);
+    var coverageGlyph = substitution.sub;
+    var pos = this.binSearch(subtable.coverage.glyphs, coverageGlyph);
+    if (pos < 0) {
+        pos = -1 - pos;
+        subtable.coverage.glyphs.splice(pos, 0, coverageGlyph);
+        subtable.substitute.splice(pos, 0, 0);
+    }
+    subtable.substitute[pos] = substitution.by;
+};
+
+/**
+ * Add or modify an alternate substitution (lookup type 1)
+ * @param {string} feature - 4-letter feature name ('liga', 'rlig', 'dlig'...)
+ * @param {Object} substitution - { sub: id, by: [ids] }
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ */
+Substitution.prototype.addAlternate = function(feature, substitution, script, language) {
+    var lookupTable = this.getLookupTables(script, language, feature, 3, true)[0];
+    var subtable = getSubstFormat(lookupTable, 1, {                // lookup type 3 subtable, format 1, coverage format 1
+        substFormat: 1,
+        coverage: { format: 1, glyphs: [] },
+        alternateSets: []
+    });
+    check.assert(subtable.coverage.format === 1, 'Ligature: unable to modify coverage table format ' + subtable.coverage.format);
+    var coverageGlyph = substitution.sub;
+    var pos = this.binSearch(subtable.coverage.glyphs, coverageGlyph);
+    if (pos < 0) {
+        pos = -1 - pos;
+        subtable.coverage.glyphs.splice(pos, 0, coverageGlyph);
+        subtable.alternateSets.splice(pos, 0, 0);
+    }
+    subtable.alternateSets[pos] = substitution.by;
+};
+
+/**
+ * Add a ligature (lookup type 4)
+ * Ligatures with more components must be stored ahead of those with fewer components in order to be found
+ * @param {string} feature - 4-letter feature name ('liga', 'rlig', 'dlig'...)
+ * @param {Object} ligature - { sub: [ids], by: id }
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ */
+Substitution.prototype.addLigature = function(feature, ligature, script, language) {
+    var lookupTable = this.getLookupTables(script, language, feature, 4, true)[0];
+    var subtable = lookupTable.subtables[0];
+    if (!subtable) {
+        subtable = {                // lookup type 4 subtable, format 1, coverage format 1
+            substFormat: 1,
+            coverage: { format: 1, glyphs: [] },
+            ligatureSets: []
+        };
+        lookupTable.subtables[0] = subtable;
+    }
+    check.assert(subtable.coverage.format === 1, 'Ligature: unable to modify coverage table format ' + subtable.coverage.format);
+    var coverageGlyph = ligature.sub[0];
+    var ligComponents = ligature.sub.slice(1);
+    var ligatureTable = {
+        ligGlyph: ligature.by,
+        components: ligComponents
+    };
+    var pos = this.binSearch(subtable.coverage.glyphs, coverageGlyph);
+    if (pos >= 0) {
+        // ligatureSet already exists
+        var ligatureSet = subtable.ligatureSets[pos];
+        for (var i = 0; i < ligatureSet.length; i++) {
+            // If ligature already exists, return.
+            if (arraysEqual(ligatureSet[i].components, ligComponents)) {
+                return;
+            }
+        }
+        // ligature does not exist: add it.
+        ligatureSet.push(ligatureTable);
+    } else {
+        // Create a new ligatureSet and add coverage for the first glyph.
+        pos = -1 - pos;
+        subtable.coverage.glyphs.splice(pos, 0, coverageGlyph);
+        subtable.ligatureSets.splice(pos, 0, [ligatureTable]);
+    }
+};
+
+/**
+ * List all feature data for a given script and language.
+ * @param {string} feature - 4-letter feature name
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ * @return {Array} substitutions - The list of substitutions.
+ */
+Substitution.prototype.getFeature = function(feature, script, language) {
+    if (/ss\d\d/.test(feature)) {               // ss01 - ss20
+        return this.getSingle(feature, script, language);
+    }
+    switch (feature) {
+        case 'aalt':
+        case 'salt':
+            return this.getSingle(feature, script, language)
+                    .concat(this.getAlternates(feature, script, language));
+        case 'dlig':
+        case 'liga':
+        case 'rlig': return this.getLigatures(feature, script, language);
+    }
+};
+
+/**
+ * Add a substitution to a feature for a given script and language.
+ * @param {string} feature - 4-letter feature name
+ * @param {Object} sub - the substitution to add (an object like { sub: id or [ids], by: id or [ids] })
+ * @param {string} [script='DFLT']
+ * @param {string} [language='dflt']
+ */
+Substitution.prototype.add = function(feature, sub, script, language) {
+    if (/ss\d\d/.test(feature)) {               // ss01 - ss20
+        return this.addSingle(feature, sub, script, language);
+    }
+    switch (feature) {
+        case 'aalt':
+        case 'salt':
+            if (typeof sub.by === 'number') {
+                return this.addSingle(feature, sub, script, language);
+            }
+            return this.addAlternate(feature, sub, script, language);
+        case 'dlig':
+        case 'liga':
+        case 'rlig':
+            return this.addLigature(feature, sub, script, language);
+    }
+};
+
+module.exports = Substitution;
