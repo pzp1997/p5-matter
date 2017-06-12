9f98de1f4e4731342d2d6e15c675088fc53ca70c
Palmer Paul <pzpaul2002@yahoo.com>
2017-06-12 12:51:47 -0400

    add mouse orbit example

---
+++ examples/mouse-orbit/mouse-orbit.js
@@ -0,0 +1,29 @@
+var moon;
+var earthImg;
+var moonImg;
+
+function preload() {
+  earthImg = loadImage("assets/earth-128.png");
+  moonImg = loadImage("assets/moon-96.png");
+}
+
+function setup() {
+  createCanvas(600, 600);
+  noCursor();
+  imageMode(CENTER);
+  fill(255);
+  noStroke();
+
+  matter.init();
+  moon = matter.makeBall(width / 2, height / 2, moonImg.width);
+}
+
+function draw() {
+  background(0, 0, 64);
+
+  var theta = atan2(mouseY - moon.getY(), mouseX - moon.getX());
+  matter.changeGravity(cos(theta), sin(theta));
+
+  image(earthImg, mouseX, mouseY);
+  image(moonImg, moon.getX(), moon.getY());
+}
