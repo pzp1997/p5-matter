b30e0450dea297b2add0c02786876ac3e5463f90
Palmer Paul <pzpaul2002@yahoo.com>
2017-06-13 10:47:34 -0400

    add freeze click example

---
+++ examples/freeze-click/freeze-click.js
@@ -0,0 +1,55 @@
+var balls = [];
+
+function setup() {
+  createCanvas(600, 600);
+
+  matter.init();
+  matter.changeGravity(0, 0.2);
+
+  for (var i = 0; i < 10; i++) {
+    addBall();
+  }
+}
+
+function addBall() {
+  balls.push(matter.makeBall(
+    random(0, width), random(-70, -100), random(40, 70), {
+      restitution: 1,
+      frictionAir: 0
+    }));
+}
+
+function mousePressed() {
+  for (var i = 0; i < balls.length; i++) {
+    var ball = balls[i];
+    if (dist(mouseX, mouseY, ball.getX(), ball.getY()) <= ball.getRadius()) {
+      if (ball.isFrozen()) {
+        ball.unfreeze();
+      } else {
+        ball.freeze();
+      }
+    }
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (var i = balls.length - 1; i >= 0; i--) {
+    var ball = balls[i];
+
+    if (ball.isFrozen()) {
+      fill(255, 0, 0);
+    } else {
+      fill(255);
+    }
+
+    ball.show();
+
+    if (ball.isOffCanvas(100)) {
+      matter.forget(ball);
+      balls.splice(i, 1);
+      addBall();
+    }
+  }
+}
