02665204709fce493e94c7ec7d16c4cbd30aaf7a
Palmer Paul <pzpaul2002@yahoo.com>
2017-06-13 12:29:52 -0400

    add pendulum example

---
+++ examples/pendulum/pendulum.js
@@ -0,0 +1,32 @@
+var ceiling;
+var bob;
+var string;
+
+function setup() {
+  var canvas = createCanvas(600, 600);
+  fill(0);
+
+  matter.init();
+
+  ceiling = matter.makeBarrier(width / 2, -100, width + 400, 240);
+
+  bob = matter.makeBall(0, 0, 70, {
+    frictionAir: 0
+  });
+
+  string = matter.connect(ceiling, bob, {
+    stiffnes: 0.8,
+    pointA: {
+      x: 0,
+      y: 120
+    }
+  });
+}
+
+function draw() {
+  background(255);
+
+  ceiling.show();
+  bob.show();
+  string.show();
+}
