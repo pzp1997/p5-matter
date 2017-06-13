5b4bb95accc3e5bccec557ad628891869153d922
Palmer Paul <pzpaul2002@yahoo.com>
2017-06-13 13:11:19 -0400

    update throw block example

--- examples/throw-block/throw-block.js
+++ examples/throw-block/throw-block.js
@@ -1,12 +1,12 @@
 var block;
 
 function setup() {
-  createCanvas(600, 600);
+  var canvas = createCanvas(600, 600);
   fill(127);
   stroke(255);
 
   matter.init();
-  matter.mouseInteraction(true);
+  matter.mouseInteraction(canvas);
 
   block = matter.makeBlock(width / 2, height / 2, 70, 70);
 
@@ -15,10 +15,14 @@ function setup() {
   boundaries are also overlapping so that I can protect the corners. The
   number 300 itself is pretty arbitrary. */
   var extra = 300;
-  matter.makeBarrier(-extra, height, width + extra, extra); // floor
-  matter.makeBarrier(-extra, -extra, width + extra, extra); // ceiling
-  matter.makeBarrier(-extra, -extra, extra, height + extra); // left wall
-  matter.makeBarrier(width, -extra, extra, height + extra); // right wall
+  var floor = matter.makeBarrier(
+    width / 2, height + extra, width + 2 * extra, 2 * extra);
+  var ceiling = matter.makeBarrier(
+    width / 2, -extra, width + 2 * extra, 2 * extra);
+  var leftWall = matter.makeBarrier(
+    -extra, height / 2, 2 * extra, height + 2 * extra);
+  var rightWall = matter.makeBarrier(
+    width + extra, height / 2, 2 * extra, height + 2 * extra);
 }
 
 function draw() {
